871558a60ab0ab0fdf563fa108bd1bd5a7679135
Krishna Pravin <krishnapravin1@gmail.com>
2020-04-29 00:18:34 +0530

    covid

---
+++ precache-manifest.ca81c13f1dfe2b370b849b1daaa9abb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f95f59cf187c2fb2f4454f22960421",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f8126813dcc9450eabb",
+    "url": "/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "cca1b086e5f33c13fe92",
+    "url": "/static/css/main.43784e4b.chunk.css"
+  },
+  {
+    "revision": "9f8126813dcc9450eabb",
+    "url": "/static/js/2.3afa83ac.chunk.js"
+  },
+  {
+    "revision": "577b7a3b9fbfa17fccf2f4ddb5a84180",
+    "url": "/static/js/2.3afa83ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cca1b086e5f33c13fe92",
+    "url": "/static/js/main.2ad1a2e2.chunk.js"
+  },
+  {
+    "revision": "20e74bb56b5aad9a2c37",
+    "url": "/static/js/runtime-main.96a74c67.js"
+  }
+]);
