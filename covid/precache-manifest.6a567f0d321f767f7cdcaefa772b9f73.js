af0ca3c246c89f54629ca88a7eaf025101e26d32
Krishna Pravin <krishnapravin1@gmail.com>
2020-04-28 23:35:58 +0530

    covid fork

---
+++ covid/precache-manifest.6a567f0d321f767f7cdcaefa772b9f73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3fc06d11a90a2c15b312f6152498b1d",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "776ce18af38517684b13",
+    "url": "/covid/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "2618041569d3c5c78387",
+    "url": "/covid/static/css/main.43784e4b.chunk.css"
+  },
+  {
+    "revision": "776ce18af38517684b13",
+    "url": "/covid/static/js/2.7e63987d.chunk.js"
+  },
+  {
+    "revision": "577b7a3b9fbfa17fccf2f4ddb5a84180",
+    "url": "/covid/static/js/2.7e63987d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2618041569d3c5c78387",
+    "url": "/covid/static/js/main.bf2d444f.chunk.js"
+  },
+  {
+    "revision": "c50f865c2f338802ec08",
+    "url": "/covid/static/js/runtime-main.678fe9b6.js"
+  }
+]);
